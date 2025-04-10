6180ebfee83f7877e87cfd132f39fec196f1df4b
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-04-10 19:17:10 +0300

    Blade Components - Star Rating Component

--- book-review/resources/views/components/star-rating.blade.php
+++ book-review/resources/views/components/star-rating.blade.php
@@ -1,3 +1,7 @@
-<div>
-    Star rating goes here!
-</div>
+@if($rating)
+    @for($i = 1; $i <= 5; $i++)
+        {{ $i <= round($rating) ? '★' : '☆' }}
+    @endfor
+@else
+    No rating yet
+@endif
